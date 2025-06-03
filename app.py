9829b6b4d2c1b095e5ee2bfbec6dc81cfca95030
gadibahar <gadiba@gmail.com>
2025-06-03 23:27:32 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-# Flask app for backend
+
 from flask import Flask
+
 app = Flask(__name__)
-@app.route('/')
-def home(): return 'Backend Loaded'
+
+@app.route("/")
+def home():
+    return "Backend is running with Gunicorn!"
